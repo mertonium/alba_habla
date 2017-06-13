6c9df89c621ff10aa6b6b7204d2c2cb98d1b9a11
John Mertens <john.mertens@gmail.com>
2017-06-13 15:51:51 -0700

    Add ability to read books

--- lib/alba_habla.rb
+++ lib/alba_habla.rb
@@ -14,17 +14,22 @@ class AlbaHabla
 
   def books
     {
-      'sam' => 'books/green_eggs_and_ham.txt',
-      'cat' => 'books/the_cat_in_the_hat.txt'
+      'sam' => 'green_eggs_and_ham.txt',
+      'cat' => 'the_cat_in_the_hat.txt',
+      'ladybird' => 'what_the_ladybird_heard.txt',
+      'ladybird2' => 'what_the_lady_bird_heard_next.txt',
+      'fox' => 'fox_in_socks.txt',
     }
   end
 
   def word_bag
-    bag = []
-    books.values.each do |book_path|
-      IO.foreach(book_path) { |line| bag << line.split('\s+')  }
+    @word_bag || begin
+      bag = []
+      books.values.each do |book_file|
+        IO.foreach(@book_path + book_file) { |line| bag << line.gsub(/[,'\.!\?]/, '').split(' ')  }
+      end
+      bag.flatten.compact.uniq
     end
-    bag.flatten.compact.uniq
   end
 
   def process_command(command)
@@ -38,17 +43,23 @@ class AlbaHabla
 
   def read(book_name)
     if (books.has_key? book_name)
-      IO.foreach(books[book_name]) do |line|
+      IO.foreach(@book_path + books[book_name]) do |line|
         puts line
-        talk line
+        talk line.gsub('"', '')
       end
     else
       talk "Sorry, I don't know that book. Ask Daddy to add it."
     end
   end
 
+  def spell(_)
+    word = word_bag.sample
+    puts word.downcase
+    talk "Let's spell #{word}."
+    talk "#{word.split('').join(' ')}"
+  end
+
   def available_subcommands
-    %w{read}
+    %w{read spell}
   end
 end
-
